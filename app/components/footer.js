ddd1d191c66ea8bd53b97a2029ce98de90c00afe
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-05-05 14:02:25 +1000

    ✨Migrate current blog to next

---
+++ app/components/footer.js
@@ -0,0 +1,8 @@
+export default function Footer() {
+    <footer id="page-footer">
+        <ul>
+            <li id="sams-footer-github-label">GitHub: <a href="https://www.github.com/SamBWagner" rel="nofollow" aria-labelledby="sams-footer-github-label" target="_blank">@SamBWagner</a></li>
+            <li id="sams-footer-linkedin-label">LinkedIn: <a href="www.linkedin.com/in/sam-b-wagner" rel="nofollow" aria-labelledby="sams-footer-linkedin-label" target="_blank">Sam Wagner</a></li>
+        </ul>
+    </footer>
+}
---
+++ app/components/header.js
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <h1 id="heading-page-name"
+                className="heading-page-name heading-name">
+                Sam Wagner
+            </h1>
+        </header>
+    );
+  }
