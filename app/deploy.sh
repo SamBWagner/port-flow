c063f4448a825f52eb03f7cc52ae0535b7a31d80
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-06-14 07:37:34 +1000

    change to pull

--- app/deploy.sh
+++ app/deploy.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 path="/var/www/port-flow"
-repository="https://github.com/SamBWagner/port-flow.git"
 
 cd $path
-git clone $repository
-
+git pull 
 yarn build
 
 pm2 restart port-flow
