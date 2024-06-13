00232ab5ce73cbc5f0b179118b972dd9a5757fdd
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-06-13 15:10:01 +1000

    migrate to next fonts

--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,20 @@
 import "./globals.css";
+import {Luckiest_Guy, Wellfleet} from "next/font/google"
 
 export const metadata = {
   title: "Sam Wagner",
   description: "Sam Wagner's personal page discussing learning Rust and game development with Bevy.",
 };
 
+const luckiestGuy = Luckiest_Guy({ subsets: ["latin"], weight: "400" });
+const wellFleet = Wellfleet({ subsets: ["latin"], weight: "400" });
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${luckiestGuy.className} ${wellFleet.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
