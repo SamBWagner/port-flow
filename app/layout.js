cb5b513e1fb037bfdc6edc6a9c265a1ec881265b
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-06-13 15:32:45 +1000

    fix font

--- app/layout.js
+++ app/layout.js
@@ -6,13 +6,21 @@ export const metadata = {
   description: "Sam Wagner's personal page discussing learning Rust and game development with Bevy.",
 };
 
-const luckiestGuy = Luckiest_Guy({ subsets: ["latin"], weight: "400" });
-const wellFleet = Wellfleet({ subsets: ["latin"], weight: "400" });
+const luckiestGuy = Luckiest_Guy({ 
+  subsets: ["latin"], 
+  weight: "400",
+  variable: "--luckiest-guy"
+ });
+const wellFleet = Wellfleet({
+   subsets: ["latin"], 
+   weight: "400",
+   variable: "--wellfleet"
+   });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${luckiestGuy.className} ${wellFleet.className}`}>
+      <body className={`${luckiestGuy.variable} ${wellFleet.variable}`}>
         {children}
       </body>
     </html>
