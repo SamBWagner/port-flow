8aeb1bd809fcc6e8578c6bebe3623586cca491dd
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-06-15 13:00:43 +1000

    Add first endpoint

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,16 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+app.UseForwardedHeaders( new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
+
+app.MapGet("/", () => "Hello world!");
 
 app.Run();
