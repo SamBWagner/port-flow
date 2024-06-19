c152fb33550eb884818265dfdb6bdc6531a59589
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-06-19 17:48:46 +1000

    💄Change fonts

--- UI/app/layout.js
+++ UI/app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import { Luckiest_Guy, Wellfleet } from "next/font/google";
+import {  Open_Sans, Montserrat } from "next/font/google";
 
 export const metadata = {
   title: "Sam Wagner",
@@ -7,21 +7,21 @@ export const metadata = {
     "Sam Wagner's personal page discussing learning Rust and game development with Bevy.",
 };
 
-const luckiestGuy = Luckiest_Guy({
+const v2323 = Montserrat({
   subsets: ["latin"],
-  weight: "400",
-  variable: "--luckiest-guy",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--montserrat",
 });
-const wellFleet = Wellfleet({
+const wellFleet = Open_Sans({
   subsets: ["latin"],
   weight: "400",
-  variable: "--wellfleet",
+  variable: "--open-sans",
 });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${luckiestGuy.variable} ${wellFleet.variable}`}>
+      <body className={`${v2323.variable} ${wellFleet.variable}`}>
         {children}
       </body>
     </html>
