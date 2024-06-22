070e6455a98d93b535509655c10cedfcc75cc15b
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-06-22 12:39:21 +1000

    no

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,10 @@ pathToUI="/var/www/port-flow/UI/"
 pathToAPI="/var/www/port-flow/API/"
 pathToLogFile="/var/www/port-flow/deploy.log"
 
-$YARN_PATH="/home/samwagner/.nvm/versions/node/v20.14.0/bin/yarn"
-
-
 cd $path || { echo "Failed to change directory to $path"; exit 1; }
 date=$(date)
+
+echo $PATH >> $pathToLogFile 2>&1
 
 if [ ! -f $pathToLogFile ]; then
     touch $pathToLogFile || { echo "Failed to create log file"; exit 1; }
@@ -27,14 +26,14 @@ if ! git pull >> $pathToLogFile 2>&1; then
 fi
 
 cd $pathToUI || { echo "Failed to change directory to $pathToUI"; exit 1; }
-if ! $YARN_PATH install >> $pathToLogFile 2>&1; then
+if ! yarn install >> $pathToLogFile 2>&1; then
     echo "ERROR: Yarn install failed" >> $pathToLogFile
     echo "Stopping deployment" >> $pathToLogFile
     echo "-----------------------------------" >> $pathToLogFile
     exit 1
 fi
 
-if ! $YARN_PATH build >> $pathToLogFile 2>&1; then
+if ! yarn build >> $pathToLogFile 2>&1; then
     echo "ERROR: Yarn build failed" >> $pathToLogFile
     echo "Stopping deployment" >> $pathToLogFile
     echo "-----------------------------------" >> $pathToLogFile
