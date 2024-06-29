fbb3014d4b9577ada2caf71f22d7f6989f3fc8d0
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-06-29 12:34:38 +1000

    remove test title

--- UI/app/components/header.js
+++ UI/app/components/header.js
@@ -4,10 +4,6 @@ export default function Header() {
       <h1 id="heading-page-name" className="container heading-page-name heading-name">
         Sam Wagner
       </h1>
-      <hr></hr>
-      <h1 id="heading-page-name" className="container heading-page-name heading-name">
-        Luke Nerd
-      </h1>
     </header>
   );
 }
