e9f3a61f7991e38a3223242655b92ea1a1622986
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2024-07-03 13:14:58 +1000

    finalise content for blog card

--- UI/app/components/blogCard.js
+++ UI/app/components/blogCard.js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "./blogCard.module.css";
 
-export default function BlogCard({ children, title, url }) {
+export default function BlogCard({ children, title, url, urlAria }) {
   return (
     <Link href={url} className={styles.cardLink}>
       <article className={styles.card}>
--- UI/app/page.js
+++ UI/app/page.js
@@ -10,14 +10,17 @@ export default function home() {
       <h2 className={`${styles.byLine}`}>Develop · Deploy · Deliver</h2>
       <hr className={`${styles.divider}`}></hr>
       <div className={`${styles.blogCardContainer}`}>
-        <BlogCard url="rust-game">
+        <BlogCard url="rust-game" urlAria="navigate to Learning Rust blog post">
           <h3>Learning Rust</h3>
           <p>
             Starting out in the industry there&apos;s been one thing I&apos;ve
             heard continually. Rust is cool, and you should learn it. So, after
             endless nagging by a few colleagues and the swathes of social media
-            that pushes it, I’ve made the plunge.
+            that pushes it, I’ve made the plunge&hellip;
           </p>
+          <div className={styles.blogCardReadMore}>
+            <p>Click to read more</p>
+          </div>
         </BlogCard>
       </div>
 
