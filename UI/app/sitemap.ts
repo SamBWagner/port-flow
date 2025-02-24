54add9295b8189a6880e6a09ecfa20da68c665cc
Sam Wagner [SSW] <samuel.b.wagner@gmail.com>
2025-02-24 17:37:44 +1000

    🔍️ Update sitemap

--- UI/app/sitemap.ts
+++ UI/app/sitemap.ts
@@ -3,19 +3,19 @@ import { MetadataRoute } from "next";
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     return [
         {
-            url: "samwagner.dev",
+            url: "https://samwagner.dev",
             lastModified: new Date(2025, 2, 24)
         },
         {
-            url: "samwagner.dev/rust-game",
+            url: "https://samwagner.dev/rust-game",
             lastModified: new Date(2023, 11, 10)
         },
         {
-            url: "samwagner.dev/first-commander-deck",
+            url: "https://samwagner.dev/first-commander-deck",
             lastModified: new Date(2023, 9, 29)
         },
         {
-            url: "samwagner.dev/fast-brew",
+            url: "https://samwagner.dev/fast-brew",
             lastModified: new Date(2024, 12, 29)
         }
     ];
